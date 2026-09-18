9695cf0087959e9eca1e8d91c9689c9b1cb70a6d
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-18 19:01:10 -0300

    Refactor GUIVenda to improve payment registration

--- lab2-aula4-aula6-polimorfismo-factory-pagamento/lab2-aula4-polimorfismo-pagamento/GUIVenda.java
+++ lab2-aula4-aula6-polimorfismo-factory-pagamento/lab2-aula4-polimorfismo-pagamento/GUIVenda.java
@@ -9,11 +9,11 @@ import Modelo.*;
 		Venda venda = new Venda(hoje);
 		CatalogoProdutos catalogo = new CatalogoProdutos();
 		ControladorRealizarVenda controladorVenda = new ControladorRealizarVenda(venda, catalogo);
+	 
 		controladorVenda.entrarItem("001", 3);
 		controladorVenda.entrarItem("002", 1);
 		controladorVenda.entrarItem("003", 2);
 		controladorVenda.registrarPagamento(500, 2);
-	 
 		System.out.println("Total = " + venda.getTotal());
 		System.out.println("Troco = " + venda.getTroco());
 		
